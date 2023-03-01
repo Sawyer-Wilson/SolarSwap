2d03dc7bc3cb73bc34999f078d5c0afce72ee896
Sawyer Wilson <sawyerianwilson@gmail.com>
2023-02-28 23:00:28 -0500

    Create EnergyListing and Offer schemas

---
+++ server/models/Offer.js
@@ -0,0 +1,21 @@
+const Mongoose = require("mongoose");
+
+const offerSchema = new Mongoose.Schema({
+	sellerID: {
+		type: Mongoose.ObjectId,
+	},
+	email: {
+		type: String,
+    required: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 255,
+    match: /^\S+@\S+\.\S+$/
+	},
+  message: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = Mongoose.model("Offer", offerSchema);
